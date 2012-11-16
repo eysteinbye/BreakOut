25eea8c1182a21b5b90618bd20a978e877cf3022
Eystein Bye <eysteinbye@gmail.com>
2012-11-16 17:07:02 +0100

    Refactoring and added pause

--- Script/game.js
+++ Script/game.js
@@ -1,23 +1,23 @@
-var gameLoop = function() {
-	board.didBallHitBlock(ball,score);
-		
-	if(board.blocksLeft == 0){
-		// Level cleared
-		score.Add(100);
-		clearInterval(gameLoopId);
-	}
-		
-	if(ball.hitLower()){
-		if(DEMO_MODE)
-			ball.bounceOfCeling();
-		else{
-			if (!ball.intersects(bar)) {
-				// if ball misses bar, then game is over
-				clearInterval(gameLoopId);
-			}
-		}
-	}
-		
-	bar.draw();
-	ball.draw();
+var gameLoop = function () {
+    board.didBallHitBlock(ball, score);
+
+    if (board.blocksLeft == 0) {
+        // Level cleared
+        score.Add(100);
+        clearInterval(gameLoopId);
+    }
+
+    if (ball.hitLower()) {
+        if (DEMO_MODE)
+            ball.bounceOfCeling();
+        else {
+            if (!ball.intersects(bar)) {
+                // if ball misses bar, then game is over
+                clearInterval(gameLoopId);
+            }
+        }
+    }
+
+    bar.draw();
+    ball.draw();
 };
--- Script/score.js
+++ Script/score.js
@@ -1,1 +1,7 @@
-var scoreObj = function(c){    this.canvas = c;    this.Score = 0; 	this.draw();};    scoreObj.prototype.Add = function (points) {        this.Score += points;        this.draw();                 return  this.Score;    };        scoreObj.prototype.draw = function() {        var top = this.canvas.Height - MARGING_UNDER_BAR + BAR_HEIGHT; // 580        var height = this.canvas.Height - top; // 20         this.canvas.Context.clearRect(10, top, 300, height);                 this.canvas.Context.strokeStyle = "#ffffff";        this.canvas.Context.font = '14px verdana';        this.canvas.Context.strokeText('Score : ' + this.Score, 10, 595);    };	/*	Lag de andre classene slik	*/
+var ScoreObj = function (c) {
+    this.canvas = c;    this.Score = 0;    this.draw();
+};ScoreObj.prototype.Add = function (points) {
+    this.Score += points;    this.draw();    return this.Score;
+};ScoreObj.prototype.draw = function () {
+    var top = this.canvas.Height - MARGING_UNDER_BAR + BAR_HEIGHT; // 580    var height = this.canvas.Height - top; // 20    this.canvas.Context.clearRect(10, top, 300, height);    this.canvas.Context.strokeStyle = "#ffffff";    this.canvas.Context.font = '14px verdana';    this.canvas.Context.strokeText('Score : ' + this.Score, 10, 595);
+};
