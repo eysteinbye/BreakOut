576289fd712160309859da0834273aec5bf6e234
Eystein Bye <eysteinbye@gmail.com>
2012-11-17 21:46:52 +0100

    Added a rude fps counter

--- Script/game.js
+++ Script/game.js
@@ -1,4 +1,20 @@
+var lastTime = new Date(2010,5,10,8,0);
+
 var gameLoop = function () {
+	
+	
+	
+	var nowTime = new Date();
+	
+	var sidenSist = timeDifference(nowTime, lastTime);
+	
+	
+	
+	
+    var ee = document.getElementById('www');
+ee.innerHTML = (1 / (sidenSist/1000));
+	lastTime = nowTime;
+	
    var block = board.didBallHitBlock(ball, score);
    if(block != null) {
        score.Add(5);
@@ -27,3 +43,18 @@ var gameLoop = function () {
 	paint.drawBall(ball);
 	
 };
+
+
+function timeDifference(endDate,startDate) {
+ var difference = endDate.getTime() - startDate.getTime();
+ //difference = Math.floor(difference/1000);
+ return difference;
+} 
+
+/*
+var startDateTime = new Date(2010,5,10,8,0);
+var endDateTime = new Date(2010,5,11,8,0); 
+ 
+var secDiff = timeSecondDifference(endDateTime,startDateTime); 
+
+*/
