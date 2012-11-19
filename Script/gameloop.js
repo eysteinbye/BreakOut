87569360517150d2ce315cb4bcd48be460b45d51
Eystein Bye <eysteinbye@gmail.com>
2012-11-19 20:53:58 +0100

    Used screen resolutions for margins and start positions

--- Script/gameloop.js
+++ Script/gameloop.js
@@ -44,11 +44,11 @@ var BAR_START_X;
 var BAR_WIDTH;
 var BAR_HEIGHT;
 
-var MARGING_UNDER_BAR = 50;
+var MARGING_UNDER_BAR;
 
 
-var BLOCKS_START_LEFT = 100;
-var BLOCKS_START_TOP = 100;
+var BLOCKS_START_LEFT;
+var BLOCKS_START_TOP;
 
 var NUMBER_OF_BLOCKROWS = 7;
 var NUMBER_OF_BLOCKS_ON_ROW = 10;
@@ -70,19 +70,36 @@ var startGame = function () {
     canvasDom.width = WIDTH;
     canvasDom.height = HEIGHT;
 
+
+
+    BLOCKS_START_LEFT = WIDTH/10;
+    BLOCKS_START_TOP = WIDTH/20;
+
+
     var widthOfAllBlocks = WIDTH - (BLOCKS_START_LEFT * 2);
     BLOCK_WIDTH = widthOfAllBlocks / NUMBER_OF_BLOCKS_ON_ROW;
 
     var heightOfAllBlocks = HEIGHT / 3;
     BLOCK_HEIGHT = heightOfAllBlocks / NUMBER_OF_BLOCKROWS;
 
+
+
+    MARGING_UNDER_BAR = BLOCK_HEIGHT;
+
+
+
     BALL_START_X = WIDTH / 2;
-    BALL_START_Y = HEIGHT / 2;
+    BALL_START_Y = HEIGHT - Math.round(MARGING_UNDER_BAR * 2);
 
     BAR_START_X = WIDTH / 2;
     BAR_HEIGHT = BLOCK_HEIGHT / 2;
     BAR_WIDTH = BLOCK_WIDTH;
 	if(BAR_WIDTH<=50) BAR_WIDTH=50;
+
+
+
+
+
 
     var context = canvasDom.getContext('2d');
     var canvas = new CanvasObj(context, canvasDom);
